a82e2edfb325c5d3e5470b59ba4b58d1eada8458
Kellen Lask <kellenlask@gmail.com>
2015-03-30 21:31:50 -0600

    Fixed the repository problem

--- app/src/main/java/com/bk/fm/breakawaygame/MainActivity.java
+++ app/src/main/java/com/bk/fm/breakawaygame/MainActivity.java
@@ -1,39 +1,55 @@
 package com.bk.fm.breakawaygame;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 
 
 public class MainActivity extends ActionBarActivity {
 
+//---------------------------------------------------
+//
+//		Fields
+//
+//---------------------------------------------------
+
+
+
+//---------------------------------------------------
+//
+//		onCreate
+//
+//---------------------------------------------------
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
+	} //End onCreate
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
+	protected void onPause() {
+
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
+	public void onDestroy() {
 
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
 	}
-}
+
+//---------------------------------------------------
+//
+//		Logistical Methods
+//
+//---------------------------------------------------
+
+
+
+//---------------------------------------------------
+//
+//		GUI Methods
+//
+//---------------------------------------------------
+	public void initializeFields() {
+
+	} //End public void initializeFields()
+
+} //End Class
