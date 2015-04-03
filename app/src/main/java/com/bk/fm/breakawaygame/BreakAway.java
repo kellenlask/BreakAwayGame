9c129fa9609ca2b10dc65efa44c33b5e30d973e9
Kellen Lask <kellenlask@gmail.com>
2015-04-02 20:55:35 -0600

    Changes

--- app/src/main/java/com/bk/fm/breakawaygame/BreakAway.java
+++ app/src/main/java/com/bk/fm/breakawaygame/BreakAway.java
@@ -1,23 +1,23 @@
 package com.bk.fm.breakawaygame;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.view.Surface;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 /**
  * Created by Kellen on 4/2/2015.
  */
-public class BreakAway extends SurfaceView implements SurfaceHolder {
+public class BreakAway extends SurfaceView implements SurfaceHolder.Callback {
 //-----------------------------------------------------------------------
 //
 //		Fields
 //
 //-----------------------------------------------------------------------
 	private Context context;
+	private Ball ball;
+	private Paddle paddle;
 
 //-----------------------------------------------------------------------
 //
@@ -29,6 +29,9 @@ public class BreakAway extends SurfaceView implements SurfaceHolder {
 		super(context, attrs);
 		this.context = context;
 
+	// register SurfaceHolder.Callback listener
+		getHolder().addCallback(this);
+
 
 	}
 
@@ -37,63 +40,59 @@ public class BreakAway extends SurfaceView implements SurfaceHolder {
 //		SurfaceView Methods
 //
 //-----------------------------------------------------------------------
+	// called when surface is first created
 	@Override
-	public void addCallback(Callback callback) {
-
+	public void surfaceCreated(SurfaceHolder holder)
+	{
+		if (!dialogIsDisplayed)
+		{
+			cannonThread = new CannonThread(holder); // create thread
+			cannonThread.setRunning(true); // start game running
+			cannonThread.start(); // start the game loop thread
+		}
 	}
 
 	@Override
-	public void removeCallback(Callback callback) {
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
 	}
 
+	// called when the surface is destroyed
 	@Override
-	public boolean isCreating() {
-		return false;
-	}
+	public void surfaceDestroyed(SurfaceHolder holder)
+	{
+		// ensure that thread terminates properly
+		boolean retry = true;
+		cannonThread.setRunning(false); // terminate cannonThread
 
+		while (retry)
+		{
+			try
+			{
+				cannonThread.join(); // wait for cannonThread to finish
+				retry = false;
+			}
+			catch (InterruptedException e)
+			{
+				System.out.println("(KELLEN) Thread Interrupted.")
+			}
+		}
+	} // end method surfaceDestroyed
+
+	// called when the user touches the screen in this Activity
 	@Override
-	public void setType(int type) {
+	public boolean onTouchEvent(MotionEvent e)
+	{
+		// get int representing the type of action which caused this event
+		int action = e.getAction();
 
-	}
+		// the user user touched the screen or dragged along the screen
+		if (action == MotionEvent.ACTION_DOWN ||
+				action == MotionEvent.ACTION_MOVE)
+		{
+			fireCannonball(e); // fire the cannonball toward the touch point
+		}
 
-	@Override
-	public void setFixedSize(int width, int height) {
-
-	}
-
-	@Override
-	public void setSizeFromLayout() {
-
-	}
-
-	@Override
-	public void setFormat(int format) {
-
-	}
-
-	@Override
-	public Canvas lockCanvas() {
-		return null;
-	}
-
-	@Override
-	public Canvas lockCanvas(Rect dirty) {
-		return null;
-	}
-
-	@Override
-	public void unlockCanvasAndPost(Canvas canvas) {
-
-	}
-
-	@Override
-	public Rect getSurfaceFrame() {
-		return null;
-	}
-
-	@Override
-	public Surface getSurface() {
-		return null;
-	}
+		return true;
+	} // end method onTouchEvent
 }
